be47dd7d96002e74f212dca7120ecc119a73b9b2
Computer <Computer@DESKTOP-GGN6R0T>
2021-04-15 20:02:11 +0200

    try catch

--- mape/src/mapeVezba/GlavnaMape.java
+++ mape/src/mapeVezba/GlavnaMape.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.print.attribute.HashAttributeSet;
 
@@ -21,10 +22,17 @@ public class GlavnaMape {
 		     osoba1.setPrezime("Cvijanovic");
 		     osoba1.setJmbg("02393874652");
 		Osoba osoba2 = new Osoba("Pera","Zdera","8237649231");
+		Osoba osoba3 = new Osoba("Pcelica","Maja","1233459231");
 		
 		List<Osoba> listaOsoba = new ArrayList<Osoba>();
 		   listaOsoba.add(osoba1); 
 		   listaOsoba.add(osoba2);
+		   
+		   try {
+			   System.out.println(listaOsoba.get(3).getIme());
+		   }catch (Exception e) {
+			   System.out.println("Nije dobar broj!");
+		   }
 		   
 		   System.out.println(listaOsoba.get(1).getIme());
 		   
@@ -34,6 +42,25 @@ public class GlavnaMape {
 		   mapaImena.put("m3", "Maja");
 		   
 		   System.out.println("Ime osobe je: " + mapaImena.get("s2"));
+		   
+		   // mapa koja nam vraca na osnovu jmbg
+		   
+		   Scanner scanner = new Scanner(System.in);
+		   System.out.println("Unesite jmbg: ");
+		   
+		   String trazeniJMBG = scanner.nextLine();
+		   Map<String, Osoba> mapaOsoba = new HashMap<String, Osoba>();
+		        mapaOsoba.put(osoba1.getJmbg(), osoba1);
+		        mapaOsoba.put(osoba2.getJmbg(), osoba2);
+		        mapaOsoba.put(osoba3.getJmbg(), osoba3);
+		        
+		        Osoba trazenaOsoba = mapaOsoba.get(trazeniJMBG);
+		        if(trazenaOsoba != null) {
+		        	System.out.println("Osoba: " + trazenaOsoba.getPrezime() + " " + trazenaOsoba.getIme() );
+		        }else {
+		        	System.out.println("Ne postoji taj kljuc!");
+		        }
+		        
 		
 		
 		
