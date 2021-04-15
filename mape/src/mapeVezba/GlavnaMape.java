4b30d7be6e204e33ce4bcc61f4b481203ced8dae
Computer <Computer@DESKTOP-GGN6R0T>
2021-04-15 18:59:12 +0200

    Poslednji karakter cifra, kreirali klasu osoba i ponovili konstruktore

--- mape/src/mapeVezba/GlavnaMape.java
+++ mape/src/mapeVezba/GlavnaMape.java
@@ -1,10 +1,35 @@
 package mapeVezba;
 
+import model.Osoba;
+
 public class GlavnaMape {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
  
+		String adresa = "Knez Mihajlova ";
+		char poslednjiKarakter = adresa.charAt(adresa.length()- 1);
+		boolean daLiJePoslednjiKarakterCifra = Character.isDigit(poslednjiKarakter);
+		if(daLiJePoslednjiKarakterCifra) {
+			System.out.println("Jeste cifra");
+		}else {
+			System.out.println("Nije cifra");
+		}
+		
+		Osoba osoba = new Osoba();
+		     osoba.setIme("Nenad");
+		     osoba.setPrezime("Cvijanovic");
+		     osoba.setJmbg("02393874652");
+		Osoba osoba2 = new Osoba("Pera","Zdera","8237649231");
+		
+		System.out.println("Ime osobe je: " + osoba2.getIme());
+		System.out.println("Prezime osobe je: " + osoba2.getPrezime());
+		
+		
+		
+		
+		
+		
 		// pozdrav za GIT!
 		
 		
